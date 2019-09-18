840c66244e76c54801c9edbde907a026b786af6f
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2019-09-18 19:27:59 -0400

    starter update

--- app/app.py
+++ app/app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, request
 import json
+import datetime
 app = Flask(__name__, template_folder="templates")
+
+date = datetime.datetime.now()
+fakeData = [
+    {
+        'ID': '23290',
+        'dateCreated': date
+    },
+    {
+        'ID': '23291',
+        'dateCreated': date
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>test</h1>"
+    return render_template('index.html', info=fakeData)
 
 
 if __name__ == '__main__':
