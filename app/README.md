d9f6ce0a5e23db7ca663b2f58fd99a8738f8bc8c
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2019-09-18 18:57:05 -0400

    added starter template

---
+++ app/README.md
@@ -0,0 +1,1 @@
+# CISC 3140 Class Project
---
+++ app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request
+import json
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return "<h1>test</h1>"
