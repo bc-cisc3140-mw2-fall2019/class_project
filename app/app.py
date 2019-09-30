f89c2c05511769fa88414c0ec97d3973bcccbc5c
sk1520 <stanislavkim16@outlook.com>
2019-09-29 21:22:57 -0400

    fixed database id to auto increment on registretion
    fixed database error where our assigned primary key wasn't auto incrementing and was giving errors. also installed bootstrap

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 from flask_sqlalchemy  import SQLAlchemy # pip install Flask-SQLAlchemy https://www.youtube.com/watch?v=Tu4vRU4lt6k
+from flask_bootstrap import Bootstrap # pip install flask-bootstrap
 from forms import FormRegister, FormLogin
 import json
 import datetime
 app = Flask(__name__, template_folder="templates")
+Bootstrap (app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:test123@127.0.0.1:3306/testdatabase'#setup a connection mysql://username:password@localhost/database https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/#connection-uri-format not sure why "+pymysql" is needed but without it, it didnt let me connect. cant find where i found the fix
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #removes warnings 
@@ -17,7 +19,7 @@ db = SQLAlchemy(app) #set db var
 
 class registerUser(db.Model): #create our database https://www.youtube.com/watch?v=cYWiDiIUxQc different video
     __tablename__ = 'user login' #name of our database table
-    id = db.Column(db.Integer, primary_key = True) # have id assigned to each user
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True) # have id assigned to each user with autoincrementing numbers
     email = db.Column(db.String(50), unique = True, nullable= False) #have a username setup to a String type max of 15 characters, have the String be unique, and make sure that the field cant be left empty which is nullable= false
     username = db.Column(db.String(15), unique = True, nullable= False) #username column with string type and holding max of 15 chars, its unique so no duplicates are registered
     password = db.Column(db.String(30), unique = False, nullable= False)# password is also a column doesnt have to be unique but cant be left empty
@@ -54,9 +56,10 @@ def register():
     if request.method == 'GET':
         return render_template('register.html',form=form)
 #----------------------------------------------------------------
-    autoincrement = 0 #need to figure out how to auto increment id's so no duplicate id is created and no error is thrown
-    registration = registerUser(id = autoincrement, email=request.form.get('email'),username=request.form.get('user'), password = request.form.get('password')) # registration variable that will hold our registration class registerUser(id, email, user, password) each field is a column in our MySQL database
-    db.session.add(registration) #we capture the session into our databse 
+   # autoincrement = 0 #need to figure out how to auto increment id's so no duplicate id is created and no error is thrown
+    registration = registerUser( email=request.form.get('email'),username=request.form.get('user'), password = request.form.get('password')) # registration variable that will hold our registration class registerUser(id, email, user, password) each field is a column in our MySQL database
+    
+    db.session.add(registration) #we capture the session into our database 
     db.session.commit() #and after we capture the session we commit it into the database
     #endtest
     return render_template('register.html', form=form) 
