75703481704e06177a838a3259a387335c56bdec
sk1520 <stanislavkim16@outlook.com>
2019-11-21 18:43:04 -0500

    added jquery function for dropdown link
    added dropdown in layout.html in nav and to make the head link in the drop down clickable with jquery

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,3 +1,20 @@
+jQuery(function($) {
+    if ($(window).width() > 769) {
+      $('.navbar .dropdown').hover(function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+  
+      }, function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+  
+      });
+  
+      $('.navbar .dropdown > a').click(function() {
+        location.href = this.href;
+      });
+  
+    }
+  });
+
 document.addEventListener("DOMContentLoaded", function () {
     console.log("hello");
 })
